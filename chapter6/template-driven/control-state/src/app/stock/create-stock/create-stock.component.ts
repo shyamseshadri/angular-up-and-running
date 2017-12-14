517a171d1be1fda1b6973aba7489b435be767aa0
Shyam Seshadri <shyamseshadri@gmail.com>
2017-12-14 11:47:27 +0530

    chapter 6: control state example done

---
+++ chapter6/template-driven/control-state/src/app/stock/create-stock/create-stock.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Stock } from 'app/model/stock';
+
+@Component({
+  selector: 'app-create-stock',
+  templateUrl: './create-stock.component.html',
+  styleUrls: ['./create-stock.component.css']
+})
+export class CreateStockComponent {
+
+  public stock: Stock;
+  public confirmed = false;
+  public exchanges = ['NYSE', 'NASDAQ', 'OTHER'];
+  constructor() {
+    this.stock =  new Stock('test', '', 0, 0, 'NASDAQ');
+  }
+
+  setStockPrice(price) {
+    this.stock.price = price;
+    this.stock.previousPrice = price;
+  }
+
+  createStock() {
+    console.log('Creating stock ', this.stock);
+  }
+}
