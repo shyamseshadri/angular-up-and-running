114c45bc6dbc6c161c68c4972430d778f9522622
Shyam Seshadri <shyamseshadri@gmail.com>
2017-12-16 12:19:18 +0530

    Chapter 6: form groups

---
+++ chapter6/template-driven/form-groups/src/app/stock/create-stock/create-stock.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Stock } from 'app/model/stock';
+
+@Component({
+  selector: 'app-create-stock',
+  templateUrl: './create-stock.component.html',
+  styleUrls: ['./create-stock.component.css']
+})
+export class CreateStockComponent {
+
+  public stock: Stock;
+  public confirmed = false;
+  public exchanges = ['NYSE', 'NASDAQ', 'OTHER'];
+  constructor() {
+    this.stock =  new Stock('', '', 0, 0, 'NASDAQ');
+  }
+
+  setStockPrice(price) {
+    this.stock.price = price;
+    this.stock.previousPrice = price;
+  }
+
+  createStock(stockForm) {
+    console.log('Stock form', stockForm.value);
+    if (stockForm.valid) {
+      this.stock = stockForm.value.stock;
+      console.log('Creating stock ', this.stock);
+    } else {
+      console.error('Stock form is in an invalid state');
+    }
+  }
+}
