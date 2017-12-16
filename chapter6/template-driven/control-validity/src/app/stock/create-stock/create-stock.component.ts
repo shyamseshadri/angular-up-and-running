34c6489e6f895343ec48bd1e6e5ee61d84b4c0ad
Shyam Seshadri <shyamseshadri@gmail.com>
2017-12-16 10:14:46 +0530

    chapter 6 control validity example

---
+++ chapter6/template-driven/control-validity/src/app/stock/create-stock/create-stock.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Stock } from 'app/model/stock';
+
+@Component({
+  selector: 'app-create-stock',
+  templateUrl: './create-stock.component.html',
+  styleUrls: ['./create-stock.component.css']
+})
+export class CreateStockComponent {
+
+  public stock: Stock;
+  public confirmed = false;
+  public exchanges = ['NYSE', 'NASDAQ', 'OTHER'];
+  constructor() {
+    this.stock =  new Stock('test', '', 0, 0, 'NASDAQ');
+  }
+
+  setStockPrice(price) {
+    this.stock.price = price;
+    this.stock.previousPrice = price;
+  }
+
+  createStock(stockForm) {
+    console.log('Stock form', stockForm);
+    if (stockForm.valid) {
+      console.log('Creating stock ', this.stock);
+    } else {
+      console.error('Stock form is in an invalid state');
+    }
+  }
+}
