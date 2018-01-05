8a22d351c314cc3bf2734219dea89c3011f2bb5c
Shyam Seshadri <shyamseshadri@gmail.com>
2018-01-05 16:18:20 +0530

    Chapter 7: Form Builder example

---
+++ chapter7/form-builder/src/app/stock/create-stock/create-stock.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-create-stock',
+  templateUrl: './create-stock.component.html',
+  styleUrls: ['./create-stock.component.css']
+})
+export class CreateStockComponent {
+
+  public stockForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.createForm();
+  }
+
+  createForm() {
+    this.stockForm = this.fb.group({
+      name: [null, Validators.required],
+      code: [null, [Validators.required, Validators.minLength(2)]],
+      price: [0, [Validators.required, Validators.min(0)]]
+    });
+  }
+
+  onSubmit() {
+    console.log('Stock Form Value', this.stockForm.value);
+  }
+}
