003b053f3c2fdf2b1e72cef580a7d1b83b80c294
Shyam Seshadri <shyamseshadri@gmail.com>
2018-01-06 13:40:21 +0530

    Chapter 7: Form control state and validity example

---
+++ chapter7/control-state-validity/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Stock } from 'app/model/stock';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'app works!';
+
+  public stock: Stock;
+
+  ngOnInit(): void {
+    this.stock = new Stock('Test Stock Company', 'TSC', 85, 80);
+  }
+
+  onToggleFavorite(stock: Stock) {
+    console.log('Favorite for stock ', stock, ' was triggered');
+    this.stock.favorite = !this.stock.favorite;
+  }
+}
