3de10ce019d3c19fe14ff24b62d56ad922065c0d
Shyam Seshadri <shyamseshadri@gmail.com>
2018-01-07 12:37:37 +0530

    Chapter 7: Form Model example

---
+++ chapter7/form-model/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { StockItemComponent } from './stock/stock-item/stock-item.component';
+import { CreateStockComponent } from './stock/create-stock/create-stock.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    StockItemComponent,
+    CreateStockComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ chapter7/form-model/src/app/stock/create-stock/create-stock.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { Stock } from 'app/model/stock';
+
+let counter = 1;
+
+@Component({
+  selector: 'app-create-stock',
+  templateUrl: './create-stock.component.html',
+  styleUrls: ['./create-stock.component.css']
+})
+export class CreateStockComponent {
+
+  private stock: Stock;
+  public stockForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.createForm();
+    this.stock = new Stock('Test ' + counter++, 'TST', 20, 10);
+  }
+
+  createForm() {
+    this.stockForm = this.fb.group({
+      name: [null, Validators.required],
+      code: [null, [Validators.required, Validators.minLength(2)]],
+      price: [0, [Validators.required, Validators.min(0)]]
+    });
+  }
+
+  loadStockFromServer() {
+    this.stock = new Stock('Test ' + counter++, 'TST', 20, 10);
+    const stockFormModel = Object.assign({}, this.stock);
+    delete stockFormModel.previousPrice;
+    delete stockFormModel.favorite;
+    this.stockForm.setValue(stockFormModel);
+  }
+
+  patchStockForm() {
+    this.stock = new Stock('Test ' + counter++, 'TST', 20, 10);
+    this.stockForm.patchValue(this.stock);
+  }
+
+  resetForm() {
+    this.stockForm.reset();
+  }
+
+  onSubmit() {
+    this.stock = Object.assign({}, this.stockForm.value);
+    console.log('Saving stock', this.stock);
+  }
+}
