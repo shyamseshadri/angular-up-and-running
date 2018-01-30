6407b0abd8a22dd3f940ac6eb200761e0cf93e88
Shyam Seshadri <shyamseshadri@gmail.com>
2018-01-30 14:33:35 +0530

    Chapter 10: Add mock and fake service tests

--- chapter10/simple-service/src/app/stock/stock-list/stock-list.component.spec.ts
+++ chapter10/simple-service/src/app/stock/stock-list/stock-list.component.spec.ts
@@ -29,6 +29,37 @@ describe('StockListComponent With Real Service', () => {
   });
 });
 
+describe('StockListComponent With Mock Service', () => {
+  let component: StockListComponent;
+  let fixture: ComponentFixture<StockListComponent>;
+  let stockService: StockService;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ StockListComponent, StockItemComponent ],
+      providers: [ StockService ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(StockListComponent);
+    component = fixture.componentInstance;
+    // Always get the Service from the Injector!
+    stockService = fixture.debugElement.injector.get(StockService);
+    let spy = spyOn(stockService, 'getStocks')
+        .and.returnValue([new Stock('Mock Stock', 'MS', 800, 900, 'NYSE')])
+    fixture.detectChanges();
+  });
+
+  it('should load stocks from mocked service on init', () => {
+    expect(component).toBeTruthy();
+    expect(component.stocks.length).toEqual(1);
+    expect(component.stocks[0].code).toEqual('MS');
+  });
+});
+
+
 describe('StockListComponent With Fake Service', () => {
   let component: StockListComponent;
   let fixture: ComponentFixture<StockListComponent>;
