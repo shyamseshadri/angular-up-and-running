d22d08c61cef97891aea34aff6a51cd22b3f5618
Shyam Seshadri <shyamseshadri@gmail.com>
2018-03-26 16:36:07 +0530

    Update chapter 2 exampl with latest angular and cli

--- chapter2/stock-market/e2e/app.po.ts
+++ chapter2/stock-market/e2e/app.po.ts
@@ -1,6 +1,6 @@
-import { browser, element, by } from 'protractor';
+import { browser, by, element } from 'protractor';
 
-export class StockMarketPage {
+export class AppPage {
   navigateTo() {
     return browser.get('/');
   }
