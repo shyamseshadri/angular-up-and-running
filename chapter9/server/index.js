243112e7193c3fc6e2add261907e6e1516eba13b
Thiên Toán <toanalien@gmail.com>
2018-09-24 16:59:19 +0700

    enable cross-origin resource sharing

--- chapter9/server/index.js
+++ chapter9/server/index.js
@@ -3,6 +3,13 @@ const bodyParser = require('body-parser');
 const app = express()
 
 app.use(bodyParser());
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.get('/api/fail', (req, res) => res.status(403).json({msg: 'You are not allowed to access this'}));
